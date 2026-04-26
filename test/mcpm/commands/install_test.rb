5af0fe67764eb129f8b86a7a9cd0d897cd9b3bc5
Jahfer Husain <echo@jahfer.com>
2026-04-26 09:13:29 -0400

    fix: add assertion back to install_test
    Use a thread-safe Queue to track install_mod! calls across
    Ori.sync fibers, then assert all declared mods were installed.

--- test/mcpm/commands/install_test.rb
+++ test/mcpm/commands/install_test.rb
@@ -13,12 +13,25 @@ module MCPM
 
       def test_invoke_installs_missing_mods
         with_mock_world do |mod_config|
-          # There are 4 declared mods in the fixture and none are installed,
-          # so install_mod! should be called once per mod.
-          Mods::ModConfig.any_instance.stubs(:install_mod!)
+          # Track which mods install_mod! is called with.
+          # We can't use mocha expects/times here because Ori.sync
+          # runs the calls concurrently and mocha isn't thread-safe.
+          installed = Queue.new
+
+          Mods::ModConfig.any_instance.stubs(:install_mod!).with do |mod_decl|
+            installed << mod_decl.project_id
+            true
+          end
 
           op = MockOpts.new(dir: mod_config.base_dir)
           @cmd.invoke(op, NAME)
+
+          installed_ids = []
+          installed_ids << installed.pop until installed.empty?
+
+          expected_ids = mod_config.mod_declarations.map(&:project_id)
+          assert_equal expected_ids.sort, installed_ids.sort,
+            "Expected install_mod! to be called for every declared mod"
         end
       end
     end
